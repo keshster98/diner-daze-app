0c1e3bfcf2bc70d102d1b08bd661fd570ea30217
Keshen <njkeshen98@gmail.com>
2026-01-15 14:40:54 +0800

    Initialize project libraries and dependencies

---
+++ app/build.gradle.kts
@@ -0,0 +1,103 @@
+plugins {
+    // Android and Kotlin
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.kotlin.compose)
+
+    // Firebase
+    alias(libs.plugins.google.gms.google.services)
+
+    // Kotlin Serialization
+    id("org.jetbrains.kotlin.plugin.serialization")
+
+    // Code Generation (KSP)
+    id("com.google.devtools.ksp")
+
+    // Dependency Injection (Hilt)
+    id("com.google.dagger.hilt.android")
+}
+
+android {
+    namespace = "com.keshen.dinerdazeapp"
+    compileSdk {
+        version = release(36)
+    }
+
+    defaultConfig {
+        applicationId = "com.keshen.dinerdazeapp"
+        minSdk = 24
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        }
+    }
+    buildFeatures {
+        compose = true
+    }
+}
+
+dependencies {
+    // AndroidX
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+
+    // Jetpack Compose
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    debugImplementation(libs.androidx.compose.ui.tooling)
+    debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    // Firebase
+    implementation(libs.firebase.firestore)
+
+    // Testing
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
+    // Lifecycle Runtime
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.10.0")
+
+    // Material 3 Icons
+    implementation ("androidx.compose.material:material-icons-extended:1.7.8")
+
+    // Navigation
+    implementation("androidx.navigation3:navigation3-runtime:1.0.0")
+    implementation("androidx.navigation3:navigation3-ui:1.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.9.0")
+
+    // Hilt
+    implementation("com.google.dagger:hilt-android:2.57.2")
+    implementation("androidx.hilt:hilt-navigation-compose:1.3.0")
+    ksp("com.google.dagger:hilt-compiler:2.57.2")
+
+    // Image Handling
+    implementation("io.coil-kt:coil-compose:2.7.0")
+    implementation("io.coil-kt.coil3:coil-compose:3.3.0")
+}
---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    // Android and Kotlin
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.android) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+
+    // Firebase
+    alias(libs.plugins.google.gms.google.services) apply false
+
+    // Code Generation (KSP)
+    id("com.google.devtools.ksp") version "2.2.21-2.0.4" apply false
+
+    // Dependency Injection (Hilt)
+    id("com.google.dagger.hilt.android") version "2.57.2" apply false
+
+    // Kotlin Serialization
+    // Serialization version needs to match Kotlin version in libs.versions.toml
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.0" apply false
+}
